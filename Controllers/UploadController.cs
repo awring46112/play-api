60be83a7b94675353aee059091696c5376bc08cf
awring46112 <awring46112@gmail.com>
2018-02-08 16:49:11 -0500

    upload

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace play_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class UploadController : Controller
+    {
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(IFormFile file)
+        {
+            System.Diagnostics.Debug.WriteLine(file.FileName);
+
+            // foreach(var formFile in files) {
+            //     if (formFile.Length > 0) {
+            //         System.Diagnostics.Debug.WriteLine(formFile.FileName);
+            //     }
+            // }
+
+            return Ok(new { Status = "Good" });
+        }
+    }
+}
